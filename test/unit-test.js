4ae6f4c3ab9d393c01e6b60bc8f683b4fb367d15
Garrick Cheung <garrick@garrickcheung.com>
2014-01-20 12:30:36 -0800

    Add round method.

--- test/unit-test.js
+++ test/unit-test.js
@@ -6,106 +6,122 @@ var refute = buster.refute
 var testCase = buster.testCase
 
 testCase('unit', {
-	setUp: function() {},
+    setUp: function() {},
 
-	'is a function': function() {
-		assert.isFunction(unit)
-	},
+    'is a function': function() {
+        assert.isFunction(unit)
+    },
 
-	'returns an object': function() {
-		assert.isObject(new unit)
-	},
+    'returns an object': function() {
+        assert.isObject(new unit)
+    },
 
-	// 'consumes a value that is set on the returned object': function() {
-	// 	var value = 123
-	// 	var obj = new unit(value)
+    // 'consumes a value that is set on the returned object': function() {
+    //  var value = 123
+    //  var obj = new unit(value)
 
-	// 	assert.equals(value, obj.value)
-	// },
+    //  assert.equals(value, obj.value)
+    // },
 
-	'method': {
-		setUp: function() {
-			this.obj = new unit()
-		},
+    'method': {
+        setUp: function() {
+            this.obj = new unit()
+        },
 
-		'check': {
-			'returns true if all requirements have been set, false otherwise': function() {
-				var value = 123
-				var to = 'gram'
-				var from = 'kilogram'
-				var obj = this.obj
+        'check': {
+            'returns true if all requirements have been set, false otherwise': function() {
+                var value = 123
+                var to = 'gram'
+                var from = 'kilogram'
+                var obj = this.obj
 
-				assert.isFalse(obj.check())
+                assert.isFalse(obj.check())
 
-				obj._value = value
-				obj._to = to
-				obj._from = from
+                obj._value = value
+                obj._to = to
+                obj._from = to
 
-				assert.isTrue(obj.check())
-			}
-		},
+                assert.isFalse(obj.check())
 
-		'to': {
-			'consumes a key referencing a definition': function() {
-				var def = 'gram'
-				var obj = this.obj
+                obj._from = from
 
-				refute.defined(obj._to)
+                assert.isTrue(obj.check())
+            }
+        },
 
-				obj.to(def)
+        'to': {
+            'consumes a key referencing a definition': function() {
+                var def = 'gram'
+                var obj = this.obj
 
-				assert.equals(obj._to, def)
-			}
-		},
+                refute.defined(obj._to)
 
-		'from': {
-			'consumes a key referencing a definition': function() {
-				var def = 'gram'
-				var obj = this.obj
+                obj.to(def)
 
-				refute.defined(obj._from)
+                assert.equals(obj._to, def)
+            }
+        },
 
-				obj.from(def)
+        'from': {
+            'consumes a key referencing a definition': function() {
+                var def = 'gram'
+                var obj = this.obj
 
-				assert.equals(obj._from, def)
-			}
-		},
+                refute.defined(obj._from)
 
-		'convert': {
-			'executes compute method when ready': function() {
-				var obj = this.obj
-				var value = 123
-				var from = 'kilogram'
-				var to = 'gram'
-				var stub = this.stub(obj, 'compute')
+                obj.from(def)
 
-				obj.to(to).from(from).convert(value)
+                assert.equals(obj._from, def)
+            }
+        },
 
-				assert.calledOnce(stub, value)
-			}
-		},
+        'convert': {
+            'executes compute method when ready': function() {
+                var obj = this.obj
+                var value = 123
+                var from = 'kilogram'
+                var to = 'gram'
+                var stub = this.stub(obj, 'compute')
 
-		// 'value / valueOf': {
-		// 	'returns the stored _value': function() {
-		// 		var obj = this.obj
-		// 		var value = 123
+                obj.to(to).from(from).convert(value)
 
-		// 		obj._value = value
+                assert.calledOnce(stub, value)
+            }
+        },
 
-		// 		assert.equals(obj.value(), value)
-		// 		assert.equals(obj.toValue(), value)
-		// 	}
-		// },
+        'value': {
+            'returns the stored _value': function() {
+                var obj = this.obj
+                var value = 123
 
-		// 'toString': {
-		// 	'returns string prepresentation of _value': function() {
-		// 		var obj = this.obj
-		// 		var value = 123
+                obj._value = value
 
-		// 		obj._value = value
+                assert.equals(obj.value(), value)
+            }
+        },
 
-		// 		assert.equals(obj.toString(), value.toString())
-		// 	}
-		// }
-	}
+        'round': {
+            'returns the rounded number from the stored _value': function() {
+                var obj = this.obj
+                var value = 0.99999
+                var significantDigits = 5
+
+                obj._value = value
+
+                assert.equals(obj.round(), 1)
+                assert.equals(obj.round(significantDigits), 0.99999)
+            }
+        }
+
+        // 'toString': {
+        //  'returns string prepresentation of _value': function() {
+        //      var obj = this.obj
+        //      var value = 123
+
+        //      obj._value = value
+
+        //      assert.equals(obj.toString(), value.toString())
+        //  }
+        // }
+    }
 })
--- unit.js
+++ unit.js
@@ -2,6 +2,8 @@
 
 var prime = require('prime')
 
+var enforcePrecision = require('mout/number/enforcePrecision')
+
 var unit = prime({
     constructor: function(definition) {
         var scope = this
@@ -18,59 +20,13 @@ var unit = prime({
         // default significant digits
         significantDigits = significantDigits != undefined ? significantDigits : 4
 
-        var scope = this
-        var value = scope._value
-        var from = scope._from
-        var factor = from.factor
-        var def = scope.definition[from]
-        var sign = ''
+        var value = this._value
 
         if (!isFinite(value) || value == 0) {
-            return value.toString()
+            return value
         }
 
-        if (value < 0) {
-            sign = '-'
-            value = -value
-        }
-
-        if (factor && 1E-10 > value) {
-            return '0'
-        }
-
-        var c = Math.floor(Math.log(value) / Math.LN10) - significantDigits + 1
-
-        if (!isFinite(c)) {
-            return sign + value
-        }
-
-        value = String( Math.round(value / Math.pow(10, c)))
-
-        if (15 < c + value.length - 1) {
-            value += 'e+' + (c + value.length - 1)
-            value = value.replace(/^([0-9])/, '$1.')
-        } else if (-15 > c + value.length - 1) {
-            value += 'e' + (c + value.length - 1)
-            value = value.replace(/^([0-9])/, '$1.')
-        } else if (c >= 0) {
-            for (c; 0 < c; c--) {
-                value += '0'
-            }
-        } else if (c = value.length - -c, c > 0) {
-            value = value.substr(0, c) + '.' + value.substring(c)
-        } else {
-            for (c; c < 0; c++) {
-                value = '0' + value
-            }
-
-            value = '0.' + value
-        }
-
-        value = value.replace(/([1-9])0+e/, "$1e")
-        value = value.replace(/(\.[0-9]*[1-9])0+$/, "$1")
-        value = value.replace(/\.0+$/, "")
-
-        return Number(sign + value)
+        return enforcePrecision(value, significantDigits)
     },
 
     // valueOf: function() {
@@ -128,10 +84,7 @@ var unit = prime({
             if (value != scope._value) {
                 scope._value = value
             }
-            // return scope.compute(scope._value, scope._from, scope._to)
         }
-        
-        // return scope
     },
 
     compute: function(value, fromType, toType) {
